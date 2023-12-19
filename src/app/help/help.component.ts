dbed00c1e43076f173adb78fae594ccdd661be92
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2023-12-19 17:24:07 +0100

    add framgents to help page urls

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,10 +1,12 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
 import { helpSections } from './data';
 import { HelpSection } from './model';
 import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute, Router } from '@angular/router';
+import { slugOf } from '../../utils';
 
 
 @Component({
@@ -22,5 +24,35 @@ import { MatCardModule } from '@angular/material/card';
 export class HelpComponent {
   sections: HelpSection[] = helpSections;
 
+  private route: ActivatedRoute = inject(ActivatedRoute);
+
+  referencedSection?: string;
+
+  constructor(private router: Router) {
+
+  }
+
+  ngOnInit() {
+    this.route.fragment.subscribe(fragment => {
+      if (fragment) {
+        this.referencedSection = fragment
+      }
+    })
+  }
+
   @ViewChild(MatAccordion) accordion!: MatAccordion;
+
+  isReferenced(section: HelpSection): boolean {
+    return this.referencedSection === slugOf(section.title)
+  }
+
+  onOpened(section: HelpSection): void {
+    this.updateFragment(slugOf(section.title))
+  }
+
+  updateFragment(value: string) {
+    if (value !== this.referencedSection) {
+      this.router.navigate([], { fragment: value })
+    }
+  }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,12 @@ export const formatTimestamp = (timestamp: string): string => {
   const result = date.toLocaleString();
   return result;
 }
+
+export const slugOf = (input: string): string => {
+  return input.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+}
