e6a04fbf66b4864b1b27f6de4c4dd0bc3e7acd14
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2023-12-27 11:12:01 +0100

    update footer style

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
+    CommonModule,
     MatButtonModule,
   ],
   templateUrl: './footer.component.html',
@@ -12,4 +14,13 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class FooterComponent {
 
+  footerOpacity?: string = "opacity: 100%;";
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    const pos = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight;
+    const max = document.documentElement.scrollHeight;
+    this.footerOpacity = `opacity: ${Math.round((100 * pos) / max)}%;`;
+    console.log(`pos: ${pos} max: ${max} footerOpacity: ${this.footerOpacity}`);
+  }
 }
