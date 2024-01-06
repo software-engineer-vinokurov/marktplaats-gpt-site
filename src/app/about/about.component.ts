f79fa3abb92dca71a160fb013a05649a88a77fa5
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2024-01-06 19:00:33 +0100

    finish about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SubscribeFormComponent } from 'negotiate-ninja-lib';
 
 @Component({
   selector: 'nn-about',
   standalone: true,
   imports: [
     RouterModule,
+    SubscribeFormComponent,
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
