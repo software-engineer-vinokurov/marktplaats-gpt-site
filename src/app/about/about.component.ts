0218aba0719fe68ed7aa68cce94b7ea2d8b693d9
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2024-01-08 17:27:52 +0100

    add dynamic CTA to about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,17 +1,50 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { SubscribeFormComponent } from 'negotiate-ninja-lib';
+import { AuthService } from '@auth0/auth0-angular';
+import { SubscribeFormComponent, RequestInviteFormComponent, InviteRequestedEvent } from 'negotiate-ninja-lib';
+import { environment } from '../../environments/environment';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+type CTAType = "signup" | "request-invite" | "subscribe";
 
 @Component({
   selector: 'nn-about',
   standalone: true,
   imports: [
     RouterModule,
+    MatIconModule,
+    MatButtonModule,
     SubscribeFormComponent,
+    RequestInviteFormComponent,
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+
+  cta!: CTAType;
+
+  constructor(public auth: AuthService,) {
+    auth.isAuthenticated$.subscribe((v) => {
+      if (v) {
+        this.cta = "subscribe";
+      } else {
+        this.cta = environment.signup_enabled ? "signup" : "request-invite";
+      }
+    });
+  }
+
+  onRequest(e: InviteRequestedEvent) {
+    console.log("NOT IMPLEMENTED", e)
+  }
+
+  onSignup() {
+    this.auth.loginWithPopup({
+      authorizationParams: {
+        screen_hint: 'signup'
+      }
+    });
+  }
 
 }
