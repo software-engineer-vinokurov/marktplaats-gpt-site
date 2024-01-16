91e9b4e2ec7baaa26f881bd4194476f0ba15fca7
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2024-01-16 13:53:50 +0100

    make toggle of sandbox sidenav with swipe

--- src/app/sandbox/sandbox.component.ts
+++ src/app/sandbox/sandbox.component.ts
@@ -21,6 +21,9 @@ import { MediaMatcher } from '@angular/cdk/layout';
 export class SandboxComponent implements OnDestroy {
   mobileQuery: MediaQueryList;
 
+  private startX?: number;
+  private endX?: number;
+
   private _mobileQueryListener: () => void;
 
   @ViewChild(MatDrawer) drawer!: MatDrawer;
@@ -41,5 +44,28 @@ export class SandboxComponent implements OnDestroy {
 
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+
+  onTouchStart(event: TouchEvent) {
+    this.startX = event.touches[0].clientX;
+  }
+
+  onTouchEnd(event: TouchEvent) {
+    this.endX = event.changedTouches[0].clientX;
+    this.handleSwipe();
+  }
+
+  handleSwipe() {
+    // Check if the swipe is to the right
+    if (this.startX && this.endX && this.endX > this.startX) {
+      // Swipe to the right
+      this.onRightSwipe();
+    }
+  }
+
+  onRightSwipe() {
+    // Your function to be called on right swipe
+    this.drawer.toggle();
   }
 }
